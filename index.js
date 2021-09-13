40149012796413d1dafdce4f577e9c2c64613fcf
270004270young1 <youngminghei@gmail.com>
2021-09-13 17:55:54 +0800

    change port

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const server = new ApolloServer({
 
     })
             
-    server.listen({port:5000})
+    server.listen({port:process.env.PORT||5000})
         .then((res)=>{
 
             console.log(`Server is running at ${res.url}`)
